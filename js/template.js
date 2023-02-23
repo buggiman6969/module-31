cfb72fdb096e282137b70aab6685c6ef17ac6cff
buggiman6969 <abdullaapon12@gmail.com>
2023-02-23 21:35:30 +0600

    done

---
+++ js/template.js
@@ -0,0 +1,15 @@
+// multiline option 1
+const multiline = "my 1st line \n" +
+"my 1st line \n" +
+"my 1st line \n" +
+"my 1st line";
+
+// console.log(multiline);
+// multiline code option 2
+
+const newMultiline = `my 1st line
+mu 2nd line
+my 3rd line
+my 4th line `;
+
+console.log(newMultiline)
